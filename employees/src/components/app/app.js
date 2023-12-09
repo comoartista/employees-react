33c043002a54111a5df49d7f6f32c7f20e44b36d
Oksana Melnyk <oks.melnuk@gmail.com>
2023-12-09 13:03:53 +0100

    worked with props & key

--- employees/src/components/app/app.js
+++ employees/src/components/app/app.js
@@ -7,6 +7,13 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
+
+    const data = [
+        {name: 'John S.', salary: 6000, increase: true, id: 1},
+        {name: 'John S.', salary: 5000, increase: false, id: 2},
+        {name: 'John S.', salary: 3000, increase: false, id: 3}
+    ];
+
     return (
         <div className="app">
         <AppInfo />
@@ -16,7 +23,7 @@ function App() {
             <AppFilter/>
         </div>
         
-        <EmployeesList/>
+        <EmployeesList data={data}/>
         <EmployeesAddForm/>
     </div>
     );
